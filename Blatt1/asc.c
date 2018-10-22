94c0599928b3fa5355dde823fa050d812bfde8af
stovemeerkat <marvin.heinzelmann@t-online.de>
2018-10-22 16:58:29 +0200

    Blatt 1: Fix formattin of the ascii table by adding zero padding to hex and oct values.

--- Blatt1/asc.c
+++ Blatt1/asc.c
@@ -4,6 +4,6 @@ int main (void) {
     unsigned int i = 0;
     printf("Oct\tDec\tHex\tChar\n");
     for(i = 0; i < 128; i++){
-        printf("%o\t%u\t%x\t%c\n",i,i,i,(char) i);
+        printf("%03o\t%u\t%02x\t%c\n",i,i,i,(char) i);
     }
 }
