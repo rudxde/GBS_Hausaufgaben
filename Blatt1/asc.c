c90246df72b4be8bbf4289ec035597b59c7db37c
rudxde <kj4derEchte@gmail.com>
2018-10-22 19:27:56 +0200

    1 - asc - fix left bound for test

--- Blatt1/asc.c
+++ Blatt1/asc.c
@@ -4,6 +4,6 @@ int main (void) {
     unsigned int i = 0;
     printf("Oct\tDec\tHex\tChar\n");
     for(i = 0; i < 128; i++){
-        printf("%03o\t%u\t%02x\t%c\n",i,i,i,(char) i);
+        printf("%03o\t%-3u\t%02x\t%c\n",i,i,i,(char) i);
     }
 }
