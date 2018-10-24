b469f338b58ebdcb9723c16bf2adaa4506b904ab
rudxde <kj4derEchte@gmail.com>
2018-10-24 10:04:02 +0200

    1 - rotup - refactor

--- Blatt1/rotup.c
+++ Blatt1/rotup.c
@@ -4,25 +4,25 @@
 #include <string.h>
 
 char charToUpperCase(char input);
-char * stringToUpperCase(const char *input, unsigned int length);
-void removeLineBreak(char *input, unsigned int length);
-char * rotup(char *input, unsigned int length);
+char * stringToUpperCase(const char *input, unsigned length);
+void removeLineBreak(char *input, unsigned length);
+char * rotStringUp(char *input, unsigned length);
 
 int main(int argc, char **argv)
 {
     char input[128];
-    unsigned int inputLen = read(0, input, 128);
-    removeLineBreak(input, inputLen);
-    char * upperInput = stringToUpperCase(input, inputLen);
-    char * rotInput = rotup(upperInput, inputLen);
+    unsigned inputLength = read(0, input, 128);
+    removeLineBreak(input, inputLength);
+    char * upperCaseInput = stringToUpperCase(input, inputLength);
+    char * inputRotUp = rotStringUp(upperCaseInput, inputLength);
 
-    unsigned int outputLen = inputLen * 2 + 13; 
+    unsigned outputLen = inputLength * 2 + 13; 
     char * output = malloc(outputLen); 
-    sprintf(output, "Hallo: %s -- %s\n", input, rotInput);
+    sprintf(output, "Hallo: %s -- %s\n", input, inputRotUp);
     write(1, output, outputLen);
 
-    free(upperInput);
-    free(rotInput);
+    free(upperCaseInput);
+    free(inputRotUp);
     free(output);
     return 0;
 }
@@ -38,20 +38,20 @@ char charToUpperCase(char input)
     return input - 32;
 }
 
-char * stringToUpperCase(const char *input, unsigned int length)
+char * stringToUpperCase(const char *input, unsigned length)
 {
     char *result = malloc(length);
-    for (unsigned int i = 0; i < length; i++)
+    for (unsigned i = 0; i < length; i++)
     {
         result[i] = charToUpperCase(input[i]);
     }
     return result;
 }
 
-void removeLineBreak(char *input, unsigned int length)
+void removeLineBreak(char *input, unsigned length)
 {
     int j = 0;
-    for (unsigned int i = 0; i < length; i++)
+    for (unsigned i = 0; i < length; i++)
     {
         if (input[i] != '\n' && input[i] != '\r')
         {
@@ -65,11 +65,12 @@ void removeLineBreak(char *input, unsigned int length)
     }
 }
 
-char * rotup(char *input, unsigned int length)
+char * rotStringUp(char *input, unsigned length)
 {
     char * output = malloc(length);
-    for (unsigned int i = 0; i < length; i++)
+    for (unsigned i = 0; i < length; i++)
     {
+        // break if char is not [A-Z]
         if(input[i] < 0x41 || input[i] > 0x5A) {
             output[i] = '\0';
             return output;
