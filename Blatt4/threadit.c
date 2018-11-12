686eab5ec233f0f453d084e7c96884fb06b4040e
stovemeerkat <marvin.heinzelmann@t-online.de>
2018-11-12 19:13:20 +0100

    4(threadit): Adapt the code that was copied from forkmany to work with threads.

--- Blatt4/threadit.c
+++ Blatt4/threadit.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+#include <pthread.h>
 #include "list.h"
 
 
@@ -11,21 +11,20 @@
 #define TRUE !FALSE
 
 int main(int argc, char **argv, char *envp[]);
-void parent(int pid);
-void child(int REnabled);
+int *child(int *countTo);
 void printDate(char prefix[]);
 int randomNext(int min, int max);
 
 int main(int argc, char **argv, char *envp[])
 {
+    int err; // used to store error codes
+
     int K = 10;
     int N = 1;
     int R = FALSE;
     int opt = 0;
-    while ((opt = getopt(argc, argv, "k:n:r")) != -1)
-    {
-        switch (opt)
-        {
+    while ((opt = getopt(argc, argv, "k:n:r")) != -1) {
+        switch (opt) {
         case 'k':
             K = atoi(optarg);
             break;
@@ -38,55 +37,51 @@ int main(int argc, char **argv, char *envp[])
         }
     }
 
-    list_t * childList = list_init();
+    list_t * threadList = list_init();
 
     // set's the seed for the random function
     srand(time(NULL));
     printDate("Start:");
 
-    for (int i = 0; i < N; i++)
-    {
+    for (int i = 0; i < N; i++) {
         int countTo = R ? randomNext(K / 2, 1.5 * K) : K;
-        int pid = fork();
-        if (pid == -1)
-        {
-            printf("childprocess could not be created!\n");
-            exit(-1);
+        pthread_t *threadId = malloc(sizeof(pthread_t));
+	if (0 != (err = pthread_create(threadId, NULL, &child, &countTo))) {
+	    fprintf(stderr, "Failed to create thread.\n");
+	    exit(err);
         }
-        else if (pid == 0)
-        {
-            // Im the child
-            child(countTo);
-        }
-        int * pidPointer = malloc(sizeof(int));
-        *pidPointer = pid;
-        list_append(childList, pidPointer);
+        list_append(threadList, threadId);
     }
-    int exitCode = 0;
-    while(childList->first != NULL) {
-        int * childPid = childList->first->data;
-        waitpid(*childPid ,&exitCode, 0);
-        free(childPid);
-        printf("Exit-Code: %i\n", WEXITSTATUS(exitCode));
-        list_remove(childList, childList->first);
+    int *exitCode;
+    while(threadList->first != NULL) {
+        pthread_t *threadId = threadList->first->data;
+	if (0 != (err = pthread_join(*threadId, &exitCode))) {
+	    fprintf(stderr, "Failed to join a thread.\n");
+	    exit(err);
+	}
+        free(threadId);
+        //printf("Exit-Code: %i\n", WEXITSTATUS(**exitCode));
+	free(exitCode);
+        list_remove(threadList, threadList->first);
     }
-
     printDate("Ende:");
     exit(0);
 }
 
-
-void child(int countTo)
+int *child(int *countTo)
 {
-    int myPid = getpid();
-    int parentPid = getppid();
+    unsigned int threadId = (unsigned int) pthread_self();
+    int pid = getpid();
     // int countTo = randomNext(K/2, 1.5*K);
-    for (unsigned i = 1; i <= countTo; i++)
-    {
+    for (int i = 1; i <= *countTo; i++) {
         sleep(1);
-        printf("%6i %6i %u\n", myPid, parentPid, i);
+        printf("%10u\t%d\t%u\n", threadId, pid, i);
     }
-    exit((myPid + countTo) % 100);
+
+    //I think we actually don't need to worry about the exit status this time, but whatever
+    int *status = malloc(sizeof(int));
+    *status = (threadId + *countTo) % 100;
+    return status;  //same as calling pthread_exit
 }
 
 void printDate(char prefix[])
