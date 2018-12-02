d77a9d4bc962498167b824f7d607332f130e1ee1
stovemeerkat <marvin.heinzelmann@t-online.de>
2018-12-02 16:01:35 +0100

    general: Turns out we were using the wrong version of list.c and list.h. This commit fixes that.

--- Blatt6/list.c
+++ Blatt6/list.c
@@ -20,7 +20,7 @@ list_t *list_init() {
  * Erzeugt ein neues Listenelement und fügt es am Beginn der Liste ein.
  * Liefert einen Zeiger auf das neue Listenelement zurück oder NULL, falls ein Fehler aufgetreten ist.
  **/
-struct list_elem *list_insert(list_t *list, list_data_t *data) {
+struct list_elem *list_insert(list_t *list, void *data) {
     if (list == NULL) {
         return NULL;
     }
@@ -41,7 +41,7 @@ struct list_elem *list_insert(list_t *list, list_data_t *data) {
  * Erzeugt ein neues Listenelement und fügt es am Ende der Liste ein.
  * Liefert einen Zeiger auf das neue Listenelement zurück oder NULL, falls ein Fehler aufgetreten ist.
  **/
-struct list_elem *list_append(list_t *list, list_data_t *data) {
+struct list_elem *list_append(list_t *list, void *data) {
     if (list == NULL) {
         return NULL;
     }
@@ -75,12 +75,18 @@ int list_remove(list_t *list, struct list_elem *elem) {
     struct list_elem *actualListElement = list->first;
     // first element should be removed
     if (actualListElement == elem) {
+        if(actualListElement == list->last) {
+            list->last = NULL;
+        }
         list->first = actualListElement->next;
         free(elem);
         return 0;
     }
     while (actualListElement != NULL) {
         if (actualListElement->next == elem) {
+            if(actualListElement->next == list->last) {
+                list->last = actualListElement;
+            }
             actualListElement->next = actualListElement->next->next;
             free(elem);
             return 0;
@@ -110,7 +116,7 @@ void list_finit(list_t *list) {
  * übergebene Funktion mit dem Argument data aus der Struktur struct list_elem auf. Für die Ausgabe werden die Elemente in
  * aufsteigender Reihefolge (beginnend bei 1) durchnumeriert.
  **/
-void list_print(list_t *list, void (*print_elem)(list_data_t *)) {
+void list_print(list_t *list, void (*print_elem)(void *)) {
     struct list_elem *actualListElement = list->first;
     unsigned counter = 1;
     while (actualListElement != NULL) {
@@ -129,7 +135,7 @@ void list_print(list_t *list, void (*print_elem)(list_data_t *)) {
  * Die Funktion muss also auf jedes Element der Liste und den übergebenen Parameter data jeweils die Vergleichsfunktion cmp_elem anwenden.
  * Eine Übereinstimmung liegt vor, wenn die Vergleichsfunktion 0 zurückliefert.
  **/
-struct list_elem *list_find(list_t *list, list_data_t *data, int (*cmp_elem)(const list_data_t *, const list_data_t *)) {
+struct list_elem *list_find(list_t *list, void *data, int (*cmp_elem)(const void *, const void *)) {
     struct list_elem *actualListElement = list->first;
     while (actualListElement != NULL) {
         int isSame = cmp_elem(data, actualListElement->data) == 0;
@@ -140,3 +146,4 @@ struct list_elem *list_find(list_t *list, list_data_t *data, int (*cmp_elem)(con
     }
     return NULL;
 }
+
--- Blatt6/list.h
+++ Blatt6/list.h
@@ -1,8 +1,6 @@
-typedef long unsigned int list_data_t;  // to change the type of the objects stored by the list, just change this typedef
-
 typedef struct list_elem {
     struct list_elem *next;  // Zeiger auf das naechste Element
-    list_data_t *data;       // Zeiger auf ein Datenobject
+    void *data;              // Zeiger auf ein Datenobject
 } list_elem_t;
 
 typedef struct list {
@@ -12,9 +10,9 @@ typedef struct list {
 
 /* function prototypes */
 list_t *list_init();
-struct list_elem *list_insert(list_t *list, list_data_t *data);
-struct list_elem *list_append(list_t *list, list_data_t *data);
+struct list_elem *list_insert(list_t *list, void *data);
+struct list_elem *list_append(list_t *list, void *data);
 int list_remove(list_t *list, struct list_elem *elem);
 void list_finit(list_t *list);
-void list_print(list_t *list, void (*print_elem)(list_data_t *));
-struct list_elem *list_find(list_t *list, list_data_t *data, int (*cmp_elem)(const list_data_t *, const list_data_t *));
+void list_print(list_t *list, void (*print_elem)(void *));
+struct list_elem *list_find(list_t *list, void *data, int (*cmp_elem)(const void *, const void *));
