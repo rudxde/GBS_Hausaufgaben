4c1b6ae374ce9b9d729154b34fc494c27d812c53
rudxde <kj4derEchte@gmail.com>
2018-12-02 23:42:11 +0100

    6(mysh): add main, try and know execute

--- Blatt6/mysh.c
+++ Blatt6/mysh.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "list.h"
+#include <unistd.h>
 #include "parser.h"
 
+#define FALSE 0
+#define TRUE !FALSE
+
+char **list_to_array(list_t *list);
+void tryExecute(char ** argv, char *envp[]);
+void knowExecute(char * path ,char ** argv, char *envp[]);
+int main(int argc, char **argv, char *envp[]);
 
 char ** list_to_array(list_t *list){
     int length = 0;
@@ -37,5 +44,36 @@ void parse_command(char **list, char *envp[]){
         command = commandpath;
     }
 }
+
+void tryExecute(char ** argv, char *envp[]) {
+    char * path = getVariableValue(envp, "PATH");
+    char * delimiter = ":";
+    char * tryPath = strtok(path, delimiter);
+    while (execve(tryPath, argv, envp) < 0){
+        tryPath = strtok(NULL, delimiter);
+        if(tryPath == NULL) {
+            printf("cannot execute '%s'", agrv[0]);
+        }
+    }
+}
+
+void knowExecute(char * path ,char ** argv, char *envp[]) {
+        if (execve(path, argv, envp) < 0){
+            printf("cannot execute '%s'", agrv[0]);
+        }
+    }
+}
+
 int main(int argc, char **argv, char *envp[]) {
+    char input[1024];
+
+    while (TRUE) {
+        printf("$ ");
+        fgets(input, 1024, stdin);
+        list_t *commandList = parse(input, envp);
+        char **args = list_to_array(commandList);
+        if (strncmp(args[0], "exit", 4) == 0) exit(0);
+        char ** newArgv list_to_array(commandList);
+
+    }
 }
