85eca94bea0c667330566da096877a36a22075b4
Alina <alina-weber7@gmx.de>
2018-12-02 23:23:52 +0100

    list_to_array feritg

--- Blatt6/mysh.c
+++ Blatt6/mysh.c
@@ -5,13 +5,26 @@
 #include "parser.h"
 
 
-(*void)[] list_to_array(list_t list){
+char ** list_to_array(list_t *list){
     int length = 0;
-    list_element_t head = list->first; 
-    while(head != null){
+    list_elem_t * head = list->first; 
+    while(head != NULL){
         length++;
         head = head->next;
     }
+    char ** array = malloc(sizeof(char *) * (length+1));
+    head = list->first;
+    int i = 0;
+    for(i = 0; i < length; i++){
+        array[i] = (char *)head;
+        head = head->next;
+    }
+    array[length] = NULL;
+
+    return array;
+
+    
 }
+
 int main(int argc, char **argv, char *envp[]) {
 }
