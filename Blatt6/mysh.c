03b958733e0c443a5944c437a96bd93c47c272f0
rudxde <kj4derEchte@gmail.com>
2018-12-02 23:53:55 +0100

    6(mysh): compile errors

--- Blatt6/mysh.c
+++ Blatt6/mysh.c
@@ -8,59 +8,56 @@
 #define TRUE !FALSE
 
 char **list_to_array(list_t *list);
-void tryExecute(char ** argv, char *envp[]);
-void knowExecute(char * path ,char ** argv, char *envp[]);
+void tryExecute(char **argv, char *envp[]);
+void knowExecute(char *path, char **argv, char *envp[]);
 int main(int argc, char **argv, char *envp[]);
 
-char ** list_to_array(list_t *list){
+char **list_to_array(list_t *list) {
     int length = 0;
-    list_elem_t * head = list->first; 
-    while(head != NULL){
+    list_elem_t *head = list->first;
+    while (head != NULL) {
         length++;
         head = head->next;
     }
-    char ** array = malloc(sizeof(char *) * (length+1));
+    char **array = malloc(sizeof(char *) * (length + 1));
     head = list->first;
     int i = 0;
-    for(i = 0; i < length; i++){
+    for (i = 0; i < length; i++) {
         array[i] = (char *)head;
         head = head->next;
     }
     array[length] = NULL;
 
     return array;
-
-    
 }
-void parse_command(char **list, char *envp[]){
+void parse_command(char **list, char *envp[]) {
     char *commandpath = list[0];
-    //Explizite Kommandopfad Angabe
-    if(strchr(commandpath, '/') != NULL){
-        char * pointer = strrchr(commandpath, '/'); 
+    // Explizite Kommandopfad Angabe
+    if (strchr(commandpath, '/') != NULL) {
+        char *pointer = strrchr(commandpath, '/');
         pointer[0] = '\0';
         list[0] = pointer[1];
         knowExecute(commandpath, list, envp);
-    }else{
+    } else {
         tryExecute(list, envp);
     }
 }
 
-void tryExecute(char ** argv, char *envp[]) {
-    char * path = getVariableValue(envp, "PATH");
-    char * delimiter = ":";
-    char * tryPath = strtok(path, delimiter);
-    while (execve(tryPath, argv, envp) < 0){
+void tryExecute(char **argv, char *envp[]) {
+    char *path = getVariableValue(envp, "PATH");
+    char *delimiter = ":";
+    char *tryPath = strtok(path, delimiter);
+    while (execve(tryPath, argv, envp) < 0) {
         tryPath = strtok(NULL, delimiter);
-        if(tryPath == NULL) {
-            printf("cannot execute '%s'", agrv[0]);
+        if (tryPath == NULL) {
+            printf("cannot execute '%s'", argv[0]);
         }
     }
 }
 
-void knowExecute(char * path ,char ** argv, char *envp[]) {
-        if (execve(path, argv, envp) < 0){
-            printf("cannot execute '%s'", agrv[0]);
-        }
+void knowExecute(char *path, char **argv, char *envp[]) {
+    if (execve(path, argv, envp) < 0) {
+        printf("cannot execute '%s'", argv[0]);
     }
 }
 
@@ -73,7 +70,6 @@ int main(int argc, char **argv, char *envp[]) {
         list_t *commandList = parse(input, envp);
         char **args = list_to_array(commandList);
         if (strncmp(args[0], "exit", 4) == 0) exit(0);
-        char ** newArgv list_to_array(commandList);
-        parse_command(newArgv, envp);
+        parse_command(args, envp);
     }
 }
