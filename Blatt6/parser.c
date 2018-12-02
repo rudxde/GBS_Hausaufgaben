cd9edaf6a0f0ada9678dfebf790170e552672c3a
rudxde <kj4derEchte@gmail.com>
2018-12-02 22:31:36 +0100

    6(parser): ...

--- Blatt6/parser.c
+++ Blatt6/parser.c
@@ -1,168 +1,181 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "list.h"
+// #include "list.h"
+#include "parser.h"
 
 #define FALSE 0
 #define TRUE !FALSE
 
-list_t *parse(char *input, char *envp[]);
-void throwWrongVariableFormat();
-char *getVariableValue(char *envp[], char name[]);
-
-typedef enum { commandtype_string, commandtype_var } commandType_t;
-
-typedef struct command {
-    commandType_t type;
-    char *variableSelector;
-    char *value;
-} command_t;
-
-int main(int argc, char **argv, char *envp[]) {
-    inputLoop(envp);
-    return 0;
-}
-
-void inputLoop(char *envp[]) {
-    char input[1024];
-    while (TRUE) {
-        fgets(input, 265, stdin);
-        parse(input, envp);
-        // TODO
-    }
-}
-
-typedef enum parseState {
-    parseState_default,
-    parseState_escaped,
-    parseState_variable,
-    parseState_singleQuoted,
-    parseState_doubleQuoted,
-    parseState_escaped_default,
-    parseState_escaped_doubleQuoted,
-    parseState_escaped_singleQuoted,
-} parseState_t;
-
 list_t *parse(char *input, char *envp[]) {
     list_t *commandList = list_init();
-    parseState_t state = parseState_default;
+    unsigned state = parseState_default;
     char *stringInProgress = malloc(sizeof(char) * 1024);
+    char *variableNameInProgress = malloc(sizeof(char) * 1024);
     int stringInProgressPointer = 0;
+    int variableInProgressPointer = 0;
     for (unsigned inputProgressPointer = 0; input[inputProgressPointer] != '\0'; inputProgressPointer++) {
         switch (input[inputProgressPointer]) {
+            case '\n':
+                break;
             case '"':
-                if (state == parseState_default)
-                    state = parseState_doubleQuoted;
-                else if (state == parseState_doubleQuoted)
-                    state = parseState_default;
-                else if (state == parseState_variable)
-                    throwWrongVariableFormat();
-                else {
+                // printf(">\" state: %u\n", state);
+                if (isState(state, parseState_default)) {
+                    state |= parseState_doubleQuoted;
+                } else if (isState(state, parseState_escaped)) {
+                    state ^= ~parseState_escaped;
                     stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
-                }
-                break;
-            case '\'':
-                if (state == parseState_default)
-                    state = parseState_singleQuoted;
-                else if (state == parseState_singleQuoted)
-                    state = parseState_default;
-                else if (state == parseState_variable)
-                    throwWrongVariableFormat();
-                else {
-                    stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
-                }
-                break;
-            case '$':
-                if (state == parseState_default)
-                    state = parseState_variable;
-                else if (state == parseState_variable)
-                    throwWrongVariableFormat();
-                else {
-                    stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
-                }
-                break;
-            case '\\':
-                if (state == parseState_default)
-                    state = parseState_escaped_default;
-                else if (state == parseState_doubleQuoted)
-                    state = parseState_escaped_doubleQuoted;
-                else if (state == parseState_singleQuoted)
-                    state = parseState_escaped_singleQuoted;
-                else if (state == parseState_variable)
-                    throwWrongVariableFormat();
-                else if (state == parseState_escaped_default) {
-                    state = parseState_default;
-                    stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
-                } else if (state == parseState_escaped_doubleQuoted) {
-                    state = parseState_doubleQuoted;
-                    stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
-                } else if (state == parseState_escaped_singleQuoted) {
-                    state = parseState_singleQuoted;
-                    stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
+                } else if (isState(state, parseState_variable)) {
+                    state &= ~parseState_variable;
+                    char *variableValue = getVariableValue(envp, variableNameInProgress);
+                    unsigned variablenamelength = strlen(variableValue);
+                    stringInProgress = appendString(stringInProgress, stringInProgressPointer, variableValue, variablenamelength);
+                    stringInProgressPointer += variablenamelength;
+                    variableInProgressPointer = 0;
+                    variableNameInProgress = malloc(sizeof(char) * 1024);
+                    if (isState(state, parseState_doubleQuoted)) {
+                        state &= ~parseState_doubleQuoted;
+                    }
+                } else if (isState(state, parseState_doubleQuoted)) {
+                    state &= ~parseState_doubleQuoted;
+                    printf("A");
                 } else {
                     stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
                 }
                 break;
+            case '\'':
+                if (isState(state, parseState_default)) {
+                    state |= parseState_singleQuoted;
+                } else if (isState(state, parseState_escaped)) {
+                    state ^= ~parseState_escaped;
+                    stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
+                } else if (isState(state, parseState_variable)) {
+                    state &= ~parseState_variable;
+                    char *variableValue = getVariableValue(envp, variableNameInProgress);
+                    unsigned variablenamelength = strlen(variableValue);
+                    stringInProgress = appendString(stringInProgress, stringInProgressPointer, variableValue, variablenamelength);
+                    stringInProgressPointer += variablenamelength;
+                    variableInProgressPointer = 0;
+                    variableNameInProgress = malloc(sizeof(char) * 1024);
+                    if (isState(state, parseState_singleQuoted)) {
+                        state &= ~parseState_singleQuoted;
+                    }
+                } else if (isState(state, parseState_singleQuoted)) {
+                    state &= ~parseState_singleQuoted;
+                } else {
+                    stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
+                }
+                break;
+            case '$':
+                if (isState(state, parseState_escaped)) {
+                    state ^= ~parseState_escaped;
+                    stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
+                } else if (isState(state, parseState_variable)) {
+                    state &= ~parseState_variable;
+                    char *variableValue = getVariableValue(envp, variableNameInProgress);
+                    unsigned variablenamelength = strlen(variableValue);
+                    stringInProgress = appendString(stringInProgress, stringInProgressPointer, variableValue, variablenamelength);
+                    stringInProgressPointer += variablenamelength;
+                    variableInProgressPointer = 0;
+                    variableNameInProgress = malloc(sizeof(char) * 1024);
+                    stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
+                } else {
+                    state |= parseState_variable;
+                }
+                break;
+            case '\\':
+                if (isState(state, parseState_escaped)) {
+                    state &= ~parseState_escaped;
+                    stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
+                } else if (isState(state, parseState_variable)) {
+                    state &= ~parseState_variable;
+                    char *variableValue = getVariableValue(envp, variableNameInProgress);
+                    unsigned variablenamelength = strlen(variableValue);
+                    stringInProgress = appendString(stringInProgress, stringInProgressPointer, variableValue, variablenamelength);
+                    stringInProgressPointer += variablenamelength;
+                    variableInProgressPointer = 0;
+                    variableNameInProgress = malloc(sizeof(char) * 1024);
+                    state |= parseState_escaped;
+                } else {
+                    state |= parseState_escaped;
+                }
+                break;
             case ' ':
-                if (state == parseState_default) {
+                if (isState(state, parseState_default)) {
                     stringInProgress[stringInProgressPointer++] = '\0';
-                    command_t *newListCommand = malloc(sizeof(command_t));
-                    newListCommand->type = commandtype_string;
-                    newListCommand->value = stringInProgress;
-                    newListCommand->variableSelector = NULL;
-                    list_append(commandList, newListCommand);
+                    list_append(commandList, stringInProgress);
                     stringInProgress = malloc(sizeof(char) * 1024);
                     stringInProgressPointer = 0;
-                } else if (state == parseState_variable) {
-                    stringInProgress[stringInProgressPointer++] = '\0';
-                    command_t *newListCommand = malloc(sizeof(command_t));
-                    newListCommand->type = commandtype_var;
-                    newListCommand->variableSelector = stringInProgress;
-                    newListCommand->value = getVariableValue(envp, stringInProgress);
-                    list_append(commandList, newListCommand);
-                    stringInProgress = malloc(sizeof(char) * 1024);
-                    stringInProgressPointer = 0;
-                } else if (state == parseState_escaped_default) {
-                    state = parseState_default;
+                } else if (isState(state, parseState_variable)) {
+                    state &= ~parseState_variable;
+                    char *variableValue = getVariableValue(envp, variableNameInProgress);
+                    unsigned variablenamelength = strlen(variableValue);
+                    stringInProgress = appendString(stringInProgress, stringInProgressPointer, variableValue, variablenamelength);
+                    stringInProgressPointer += variablenamelength;
+                    variableInProgressPointer = 0;
+                    variableNameInProgress = malloc(sizeof(char) * 1024);
                     stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
-                } else if (state == parseState_escaped_doubleQuoted) {
-                    state = parseState_doubleQuoted;
-                    stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
-                } else if (state == parseState_escaped_singleQuoted) {
-                    state = parseState_singleQuoted;
+                } else if (isState(state, parseState_escaped)) {
+                    state &= ~parseState_escaped;
                     stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
                 } else {
                     stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
                 }
                 break;
             default:
-                if (state == parseState_escaped_default) {
-                    state = parseState_default;
+                if (isState(state, parseState_escaped)) {
+                    state &= ~parseState_escaped;
                     stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
-                } else if (state == parseState_escaped_doubleQuoted) {
-                    state = parseState_doubleQuoted;
-                    stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
-                } else if (state == parseState_escaped_singleQuoted) {
-                    state = parseState_singleQuoted;
-                    stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
+                } else if (isState(state, parseState_variable)) {
+                    if (isVariableValid(input[inputProgressPointer])) {
+                        variableNameInProgress[variableInProgressPointer++] = input[inputProgressPointer];
+                    } else {
+                        state &= ~parseState_variable;
+                        char *variableValue = getVariableValue(envp, variableNameInProgress);
+                        unsigned variablenamelength = strlen(variableValue);
+                        stringInProgress = appendString(stringInProgress, stringInProgressPointer, variableValue, variablenamelength);
+                        stringInProgressPointer += variablenamelength;
+                        variableInProgressPointer = 0;
+                        variableNameInProgress = malloc(sizeof(char) * 1024);
+                        stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
+                    }
                 } else {
                     stringInProgress[stringInProgressPointer++] = input[inputProgressPointer];
                 }
                 break;
         }
     }
+    char *variableValue = getVariableValue(envp, variableNameInProgress);
+    unsigned variablenamelength = strlen(variableValue);
+    stringInProgress = appendString(stringInProgress, stringInProgressPointer, variableValue, variablenamelength);
+    stringInProgressPointer += variablenamelength;
+    stringInProgress[stringInProgressPointer++] = '\0';
+    list_append(commandList, stringInProgress);
     return commandList;
 }
 
-void throwWrongVariableFormat() {
-    printf("");
-    exit(-1);
+unsigned isVariableValid(char isValid) {
+    if (isValid >= 'A' && isValid <= 'Z') return TRUE;
+    if (isValid >= '0' && isValid <= '9') return TRUE;
+    if (isValid == '_') return TRUE;
+    return FALSE;
+}
+
+char *appendString(char *str1, unsigned strlen1, char *str2, unsigned strlen2) {
+    char *result = malloc(sizeof(char) * 1024);
+    for (unsigned i = 0; i < strlen1; i++) {
+        result[i] = str1[i];
+    }
+    for (unsigned i = strlen1; i < strlen1 + strlen2; i++) {
+        result[i] = str2[i - strlen1];
+    }
+    return result;
 }
 
 char *getVariableValue(char *envp[], char name[]) {
     for (int i = 0; envp[i] != NULL; i++) {
         size_t arglen = strlen(envp[i]);
-        char * varname = malloc(sizeof(char) * arglen);
+        char *varname = malloc(sizeof(char) * arglen);
         int j = 0;
         char current = envp[i][j];
         while ('=' != current) {
@@ -173,8 +186,8 @@ char *getVariableValue(char *envp[], char name[]) {
         varname[j] = '\0';
         j++;
 
-        if (0 == strcmp(varname, name)) {  // we found the evironment variable with the right name
-            char* value = malloc(arglen * sizeof(char));  // TODO: make sure this is freed up at some point (should be fine though)
+        if (0 == strcmp(varname, name)) {                 // we found the evironment variable with the right name
+            char *value = malloc(arglen * sizeof(char));  // TODO: make sure this is freed up at some point (should be fine though)
             int k = 0;
             current = envp[i][j];
             while ('\0' != current) {
@@ -190,5 +203,12 @@ char *getVariableValue(char *envp[], char name[]) {
         free(varname);
     }
     // no environment variable with the right name exists
-    return NULL;  // TODO: reconsider, whether this is smart or not
+    char *emptyStr = malloc(sizeof(char));
+    emptyStr[0] = '\0';
+    return emptyStr;
+}
+
+unsigned isState(unsigned is, parseState_t state) {
+    if (state == parseState_default) return is == state;
+    return (is & state) == state;
 }
---
+++ Blatt6/parsertest.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// #include "list.h"
+#include "parser.h"
+
+#define FALSE 0
+#define TRUE !FALSE
+
+int main(int argc, char **argv, char *envp[]) {
+    char input[1024];
+    unsigned running = TRUE;
+    while (running) {
+        printf("$ ");
+        fgets(input, 1024, stdin);
+        list_t *commandList = parse(input, envp);
+        list_elem_t *currentElement = commandList->first;
+        if (strncmp((char *)currentElement->data, "exit", 4) == 0) running = FALSE;
+        for (unsigned i = 1; currentElement != NULL; i++) {
+            printf("%u:%s\n", i, (char *)currentElement->data);
+            free(currentElement->data);
+            currentElement = currentElement->next;
+        }
+    }
+}
